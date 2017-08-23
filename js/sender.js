64615a6d2c53ec15a9539a7c6961dc1b119d5056
Newton Ni <nwtnni@gmail.com>
2017-08-23 12:27:41 -0400

    Refactored code and added teams

--- js/sender.js
+++ js/sender.js
@@ -3,7 +3,14 @@ $(function() {
     var resetNamespace = "urn:x-cast:codenames.reset";
     var updateNamespace = "urn:x-cast:codenames.update";
     var session = null;
+
     var words = null;
+    var blueTeam = null;
+    var redTeam = null;
+    var assassin = null;
+
+    var currentBlue = null;
+    var selectedCards = null;
 
     if (!chrome.cast || !chrome.cast.isAvailable) {
         setTimeout(initializeCastApi, 1000);
@@ -21,41 +28,63 @@ $(function() {
 
     function onInitSuccess() {
         console.log("Successfully initialized");
-        resetBoard();
     }
 
     function onError(message) {
         console.log(message);
     }
 
-   	function resetBoard() {
+    function initializeBoard() {
+    	resetWords();
+    	resetTeams();
+    	selectedCards = [];
+
+    	displayCurrent();
+    	displayWords();
+    	displayTeams();
+    }
+
+    function resetTeams() {
+    	$(".card").removeClass("blue-team red-team assassin");
+    	var seed = _.shuffle(_.range(25));
+    	currentBlue = (Math.random() > 0.5);
+
+    	blueTeam = seed.splice(0, 7);
+    	redTeam = seed.splice(7, 7);
+    	assassin = seed.splice(14, 1);
+    	currentBlue ? blueTeam.push(seed[15]) : redTeam.push(seed[15]);
+    }
+
+   	function resetWords() {
         fetch("../words.txt").then(function(response) {
             response.text().then(function(text) {
-                words = shuffle(text.split("\n")).splice(200, 25);
-                display(words);
+                words = _.shuffle(text.split("\n")).splice(200, 25);
+        		$(".card").text("");
             });
         });   		
    	}
 
-   	function display(words) {
+   	function displayCurrent() {
+   		$(".team-indicator").removeClass().addClass(currentBlue ? "blue-team" : "red-team");
+   	}
+
+   	function displayTeams() {
+   		$(".card").addClass(function(index, currentClassName) {
+   			if (blueTeam.includes(index)) {
+   				return "blue-team";
+   			} else if (redTeam.includes(index)) {
+   				return "red-team";
+   			} else if (assassin == index) {
+   				return "assassin";
+   			} 
+   		});
+   	}
+
+   	function displayWords() {
    		$(".card").each(function(index) {
     		this.textContent = words[index];
     	});
    	}
 
-    function shuffle(arr) {
-        var i = 0;
-        var j = 0;
-        var temp = null;
-
-        for (i = arr.length - 1; i > 0; i -= 1) {
-            j = Math.floor(Math.random() * (i + 1));
-            temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-        return arr;
-    }
-
-    resetBoard();
+    initializeBoard();
 });
