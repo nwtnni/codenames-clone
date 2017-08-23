5eff772c69200c971ec99bb4b4027f1a33f0f6f6
Newton Ni <nwtnni@gmail.com>
2017-08-22 20:51:37 -0400

    First test of board initialization

--- js/sender.js
+++ js/sender.js
@@ -1,37 +1,60 @@
 $(function() {
-	var appID = "28E04352";
-	var resetNamespace = "urn:x-cast:codenames.reset";
-	var updateNamespace = "urn:x-cast:codenames.update";
-	var session = null;
+    var appID = "28E04352";
+    var resetNamespace = "urn:x-cast:codenames.reset";
+    var updateNamespace = "urn:x-cast:codenames.update";
+    var session = null;
 
-	if (!chrome.cast || !chrome.cast.isAvailable) {
-		setTimeout(initializeCastApi, 1000);
-	}
+    if (!chrome.cast || !chrome.cast.isAvailable) {
+        setTimeout(initializeCastApi, 1000);
+    }
 
-	function initializeCastApi() {
-		var request = new chrome.cast.SessionRequest(appID);
-		var apiConfig = new chrome.cast.ApiConfig(request, sessionListener, function(){});
-		chrome.cast.initialize(apiConfig, onInitSuccess, onError);
-	}
+    function initializeCastApi() {
+        var request = new chrome.cast.SessionRequest(appID);
+        var apiConfig = new chrome.cast.ApiConfig(request, sessionListener, function() {});
+        chrome.cast.initialize(apiConfig, onInitSuccess, onError);
+    }
 
-	function sessionListener(e) {
-		session = e;
-	}
+    function sessionListener(e) {
+        session = e;
+    }
 
-	function onInitSuccess() {
-		console.log("Successfully initialized");
-		resetBoard();
-	}
+    function onInitSuccess() {
+        console.log("Successfully initialized");
+        resetBoard();
+    }
 
-	function onError(message) {
-		console.log(message);
-	}
+    function onError(message) {
+        console.log(message);
+    }
 
-	function resetBoard() {
-		fetch("../words.txt").then(function(response) {
-			console.log(response.text());
-		});
-	}
+    function resetBoard() {
+    	var words = loadWords();
+    	$("card").each(function(index) {
+    		this.textContent = words[index];
+    	});
+    }
 
-	resetBoard();
+   	function loadWords() {
+        fetch("../words.txt").then(function(response) {
+            var words = response.text().then(function(text) {
+                return shuffle(text.split("\n"));
+            });
+            return words.splice(0, 25);
+        });   		
+   	}
+
+    function shuffle(arr) {
+        var i = 0;
+        var j = 0;
+        var temp = null;
+
+        for (i = array.length - 1; i > 0; i -= 1) {
+            j = Math.floor(Math.random() * (i + 1));
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+    resetBoard();
 });
