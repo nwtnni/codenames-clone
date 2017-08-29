0967da5462997768b99127837c9d5509a1d035ee
Newton Ni <nwtnni@gmail.com>
2017-08-29 00:17:18 -0400

    Factored out a Board class for future JSONifying and maintainability

--- js/receiver.js
+++ js/receiver.js
@@ -1,18 +1,12 @@
 $(function() {
-    cast.receiver.logger.setLevelValue(cast.receiver.LoggerLevel.DEBUG);
 
-    window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
-    window.messageBus = window.castReceiverManager.getCastMessageBus("codenames-clone");
+    var namespace = "urn:x-cast:codenames.update";
 
-    function initialize(words) {
+    var manager = cast.receiver.CastReceiverManager.getInstance();
+    var resetBus = manager.getCastMessageBus(resetNamespace);
+    var updateBus = manager.getCastMessageBus(updateNamespace);
 
-    }
-
-    window.messageBus.onMessage = function(message) {
-        
-    };
+   	manager.start();
 
 
-
-    window.castReceiverManager.start({ statusText: 'Application is starting' });
 });
--- js/sender.js
+++ js/sender.js
@@ -1,16 +1,20 @@
 $(function() {
     var appID = "28E04352";
-    var resetNamespace = "urn:x-cast:codenames.reset";
-    var updateNamespace = "urn:x-cast:codenames.update";
+    var namespace = "urn:x-cast:codenames.update";
     var session = null;
+    var board = null;
 
-    var words = null;
-    var blueTeam = null;
-    var redTeam = null;
-    var assassin = null;
+    function Board(words) {
+        this.words = words;
+        this.selectedCards = [];
 
-    var currentBlue = null;
-    var selectedCards = null;
+        var seed = _.shuffle(_.range(25));
+        this.currentBlue = (Math.random() > 0.5);
+        this.blue = seed.splice(0, 7);
+        this.red = seed.splice(0, 7);
+        this.assassin = seed.splice(0, 1);
+        this.currentBlue ? this.blue.push(seed[0]) : this.red.push(seed[0]);
+    }
 
     if (!chrome.cast || !chrome.cast.isAvailable) {
         setTimeout(initializeCastApi, 1000);
@@ -34,51 +38,48 @@ $(function() {
         console.log(message);
     }
 
-    function initializeBoard() {
-        resetWords();
-        resetTeams();
-        selectedCards = [];
-
-        displayCurrent();
-        displayTeams();
+    function initialize() {
+      clearBoard();
+      createBoard();
     }
 
-    function resetTeams() {
+    function clearBoard() {
+        $(".card").text("");
         $(".card").removeClass("blue-team red-team assassin");
-        var seed = _.shuffle(_.range(25));
-        currentBlue = (Math.random() > 0.5);
-
-        blueTeam = seed.splice(0, 7);
-        redTeam = seed.splice(0, 7);
-        assassin = seed.splice(0, 1);
-        currentBlue ? blueTeam.push(seed[0]) : redTeam.push(seed[0]);
     }
 
-    function resetWords() {
-        fetch("../words.txt").then(function(response) {
+    function createBoard() {
+      fetch("../words.txt").then(function(response) {
             response.text().then(function(text) {
                 parseWords(text);
-                $(".card").text("");
-                displayWords();
+                displayBoard();
             });
         });
     }
 
+    function displayBoard() {
+      displayCurrent();
+      displayTeams();
+      displayWords();
+    }
+
     function parseWords(text) {
-        words = _.shuffle(text.split("\n")).splice(200, 25);
+        var words = _.shuffle(text.split("\n")).splice(200, 25);
+        board = new Board(words);
     }
 
     function displayCurrent() {
-        $(".team-indicator").removeClass().addClass(currentBlue ? "blue-team" : "red-team");
+        $(".team-indicator").removeClass().addClass(board.currentBlue ? "blue-team" : "red-team");
+        $(".team-indicator").text(board.currentBlue? "Blue Team" : "Red Team");
     }
 
     function displayTeams() {
         $(".card").addClass(function(index, currentClassName) {
-            if (blueTeam.includes(index)) {
+            if (board.blue.includes(index)) {
                 return "blue-team";
-            } else if (redTeam.includes(index)) {
+            } else if (board.red.includes(index)) {
                 return "red-team";
-            } else if (assassin.includes(index)) {
+            } else if (board.assassin.includes(index)) {
                 return "assassin";
             }
         });
@@ -86,9 +87,9 @@ $(function() {
 
     function displayWords() {
         $(".card").each(function(index) {
-            this.textContent = words[index];
+            this.textContent = board.words[index];
         });
     }
 
-    initializeBoard();
+    initialize();
 });
